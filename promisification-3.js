ced5028aa8799833033ca30514cff483187af6d4
Semenova-Daria <g.woman.one@gmail.com>
2021-08-19 02:02:35 +0300

    hw-11-3

--- promisification-3.js
+++ promisification-3.js
@@ -15,7 +15,7 @@ const makeTransaction = (transaction) => {
     const canProcess = Math.random() > 0.3;
 
     if (canProcess) {
-      resolve(transaction.id, delay);
+      resolve({ id: transaction.id, time: delay });
     } else {
       rejected(transaction.id);
     }
@@ -23,7 +23,7 @@ const makeTransaction = (transaction) => {
 });
 };
 
-const logSuccess = (id, time) => {
+const logSuccess = ({ id, time }) => {
   console.log(`Transaction ${id} processed in ${time}ms`);
 };
 
