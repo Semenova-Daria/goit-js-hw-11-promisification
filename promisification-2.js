8113bc48015c1b65c1c7710807bed05d7080a727
Semenova-Daria <g.woman.one@gmail.com>
2021-08-19 01:50:17 +0300

    promise

---
+++ promisification-2.js
@@ -0,0 +1,55 @@
+// task-2
+
+// Перепиши функцию toggleUserState() так,
+//     чтобы она не использовала callback -
+//     функцию callback, а принимала всего два параметра
+// allUsers и userName и возвращала промис.
+
+const users = [
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
+];
+
+// const toggleUserState = (allUsers, userName, callback) => {
+//   const updatedUsers = allUsers.map(user =>
+//     user.name === userName ? { ...user, active: !user.active } : user,
+//   );
+
+//   callback(updatedUsers);
+// };
+
+// const toggleUserState = (allUsers, userName) => {
+//   const updatedUsers = allUsers.map(user =>
+//     user.name === userName ? { ...user, active: !user.active } : user,
+//   );
+
+//   return Promise.resolve(updatedUsers);
+// };
+
+const toggleUserState = async(allUsers, userName) => {
+  const updatedUsers = allUsers.map(user =>
+    user.name === userName ? { ...user, active: !user.active } : user,
+  );
+
+  return updatedUsers;
+};
+
+// const toggleUserState = (allUsers, userName) => {
+//     return new Promise((resolved) => {
+//         const updatedUsers = allUsers.map(user =>
+//             user.name === userName ? { ...user, active: !user.active } : user,
+//         );
+//         resolved(updatedUsers);
+//     });
+        
+// }
+
+const logger = updatedUsers => console.table(updatedUsers);
+
+/*
+ * Должно работать так
+ */
+toggleUserState(users, 'Mango').then(logger);
+toggleUserState(users, 'Lux').then(logger);
